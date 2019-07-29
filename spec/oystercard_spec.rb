0bccf022a2c99c3364b340828d4eb3314bc821b9
Saidolimkhon Akmalov <saidolimhona12@gmail.com>
2019-07-29 16:02:22 +0100

    Step 5 Top up feature added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,5 +3,8 @@ describe Oystercard do
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
+  it 'Add money to card' do
+    expect{ subject.top_up(5) }.to change { subject.balance }.by(5)
+  end
 end
 
